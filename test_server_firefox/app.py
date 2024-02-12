d41e07da2da4aea5ef1472776889926b2aefbfa3
Hongbae Kim <hongbaki@c4c4c3.42berlin.de>
2024-02-12 14:30:43 +0100

    test

--- test_server_firefox/app.py
+++ test_server_firefox/app.py
@@ -512,7 +512,7 @@ def handle_form():
 
 if __name__ == '__main__':  
     # app.run(host='0.0.0.0', port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, debug=False)
 
 
 
