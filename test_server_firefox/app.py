9bc3d03c27917ee908f726204653d1bcdbd524b4
Hongbae Kim <hongbaki@c4c2c6.42berlin.de>
2024-02-14 15:11:06 +0100

    Test

--- test_server_firefox/app.py
+++ test_server_firefox/app.py
@@ -222,7 +222,7 @@ def handle_form():
 
     # This flag will indicate whether a slot has been successfully clicked
     slot_clicked = False
-    max_retries = 200
+    max_retries = 300
     attempts = 0
 
 
