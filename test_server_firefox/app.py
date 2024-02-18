68731ef3a90f1058cb8fdc27382020ad3c4d4b89
Hongbae Kim <hongbaki@c4c3c6.42berlin.de>
2024-02-18 14:11:18 +0100

    Test

--- test_server_firefox/app.py
+++ test_server_firefox/app.py
@@ -327,10 +327,10 @@ def handle_form():
     driver.quit()
     return render_template('index.html')
 
-# # SocketIO event to handle the attempt count
-# @socketio.on('attempt_count_request')
-# def handle_attempt_count_request():
-#     emit('attempt_count', {'attempt': session.get('attempts', 0)})
+# SocketIO event to handle the attempt count
+@socketio.on('attempt_count_request')
+def handle_attempt_count_request():
+    emit('attempt_count', {'attempt': session.get('attempts', 0)})
 
 @socketio.on('progress_update')
 def handle_progress_update():
