f32f335a8d2ed1f965e96324afe9fc89b3e3f8e8
HONGBAEKIM <hongpei17@gmail.com>
2024-03-12 20:20:28 +0100

    Test

--- test_server_ch/app.py
+++ test_server_ch/app.py
@@ -37,7 +37,7 @@ from flask_session import Session
 
 import undetected_chromedriver as uc 
 
-options = uc.ChromeOptions()
+options = webdriver.ChromeOptions()
 # localhost_number = random.randint(65536, 65999)
 # options.add_experimental_option("debuggerAddress", f"localhost:{localhost_number}")
 options.add_argument("--headless")
