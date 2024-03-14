f097f0ba6949491be1491c3d1fc4436ba8c93804
Hongbae Kim <hongbaki@c4c5c3.42berlin.de>
2024-03-14 08:57:57 +0100

    test

--- test_server_ch/app.py
+++ test_server_ch/app.py
@@ -136,7 +136,7 @@ def attempt_login(driver, username, password):
 def attempt_project(full_url):
     try:
         # Wait for navigation and check if the login was successfuld
-        WebDriverWait(driver, 1).until(EC.url_to_be(full_url))
+        # WebDriverWait(driver, 1).until(EC.url_to_be(full_url))
         if (full_url == "https://profile.intra.42.fr/"):
             driver.quit()
             return False
