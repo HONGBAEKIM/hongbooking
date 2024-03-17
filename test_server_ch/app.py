10996bb72497a90a639c6932d4df9b9a3953ba6c
Hongbae Kim <hongbaki@c4c2c3.42berlin.de>
2024-03-17 13:54:25 +0100

    Test

--- test_server_ch/app.py
+++ test_server_ch/app.py
@@ -147,7 +147,7 @@ def attempt_login(driver, username, password):
         #password_field.send_keys(Keys.ENTER)
     
         # Wait for navigation and check if the login was successfuld
-        # WebDriverWait(driver, 1).until(EC.url_to_be("https://profile.intra.42.fr/"))
+        WebDriverWait(driver, 1).until(EC.url_to_be("https://profile.intra.42.fr/"))
         print("000000000!!!!!!!!!!!!!!!!!!!logged_in")
         
         return True  # Return True to indicate successful login
