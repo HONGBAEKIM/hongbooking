249f645f512b41bfef1fedc7e3baee04c50ed5c3
Hongbae Kim <hongbaki@c4c6c3.42berlin.de>
2024-03-18 22:19:06 +0100

    Test

--- hongpage/app.py
+++ hongpage/app.py
@@ -1,29 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hongbooking')
+def hongbooking():
+    return render_template('hongbooking.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-# from flask import Flask, render_template
 
-# app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Hello, Flask!"
 
-# # @app.route('/')
-# # def index():
-# #     return render_template('index.html')
 
-# # @app.route('/hongbooking')
-# # def hongbooking():
-# #     return render_template('hongbooking.html')
+
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0')
