17cfac5730404401e37bc2cac2ab31882d57331d
Hongbae Kim <hongbaki@c4c6c3.42berlin.de>
2024-03-18 20:51:48 +0100

    Test

--- hongpage/app.py
+++ hongpage/app.py
@@ -7,7 +7,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 # from flask import Flask, render_template
 
