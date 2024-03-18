20400dcebd49a983daae3596db17807c20b054f6
Hongbae Kim <hongbaki@c4c6c3.42berlin.de>
2024-03-18 19:56:05 +0100

    Test

--- hongpage/app.py
+++ hongpage/app.py
@@ -11,4 +11,6 @@ def hongbooking():
     return render_template('hongbooking.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    
+    #app.run(host='0.0.0.0', port=5000, debug=True)
