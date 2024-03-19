7f32182be90cb4fb45d0f76a3c8e79002c6d68ba
Hongbae Kim <hongbaki@c4c1c3.42berlin.de>
2024-03-19 16:57:30 +0100

    test

--- hongpage/app.py
+++ hongpage/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
@@ -9,6 +9,13 @@ def index():
 @app.route('/hongbooking')
 def hongbooking():
     return render_template('hongbooking.html')
+
+@app.route('/submit_booking', methods=['POST'])
+def submit_booking():
+    # Handle form submission logic here
+    # For example, you can access form data using request.form
+    # Process the form data and return a response
+    return 'Booking submitted successfully'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
