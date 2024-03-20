6b1289895e6d2f5ab92b213dfe37aa78e22f7f5a
HONGBAEKIM <hongpei17@gmail.com>
2024-03-20 22:27:50 +0100

    added cgi-bin

---
+++ cgi-bin/download.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+from http.server import BaseHTTPRequestHandler
+
+class DownloadRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Specify the file you want to serve
+        filepath = 'tmp/to/your/downloadfile.txt'
+        filename = os.path.basename(filepath)
+        
+        # Check if file exists
+        if os.path.isfile(filepath):
+            # Set headers
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/octet-stream')
+            self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
+            self.end_headers()
+            
+            # Read the file and write it to response
+            with open(filepath, 'rb') as file:
+                self.wfile.write(file.read())
+        else:
+            # If file not found, send 404 response
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'File not found.')
+
+if __name__ == "__main__":
+    from http.server import HTTPServer
+    server_address = ('', 8000)  # Serve at: http://localhost:8000
+    httpd = HTTPServer(server_address, DownloadRequestHandler)
+    print("Server started on localhost port 8000...")
+    httpd.serve_forever()
