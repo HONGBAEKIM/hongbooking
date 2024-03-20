d4fdd3886266042b9668cbaca6a3a7d5514d1ef0
HONGBAEKIM <hongpei17@gmail.com>
2024-03-21 00:17:45 +0100

    added log

--- cgi-bin/download_p.py
+++ cgi-bin/download_p.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 
 import os
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import ssl
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class DownloadRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Specify the file you want to serve
-        filepath = '/home/ubuntu/2booking/cgi-bin/downloadfiles/hongbooking19_piscine'
-        filename = os.path.basename(filepath)
-        
-        # Check if file exists
-        if os.path.isfile(filepath):
-            # Set headers
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/octet-stream')
-            self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
-            self.end_headers()
+        try:
+            # Specify the file you want to serve
+            filepath = '/home/ubuntu/2booking/cgi-bin/downloadfiles/hongbooking19_piscine'
+            filename = os.path.basename(filepath)
             
-            # Read the file and write it to response
-            with open(filepath, 'rb') as file:
-                self.wfile.write(file.read())
-        else:
-            # If file not found, send 404 response
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b'File not found.')
+            # Check if file exists
+            if os.path.isfile(filepath):
+                # Set headers
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/octet-stream')
+                self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
+                self.end_headers()
+                
+                # Read the file and write it to response
+                with open(filepath, 'rb') as file:
+                    self.wfile.write(file.read())
+            else:
+                # If file not found, send 404 response
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(b'File not found.')
+        except Exception as e:
+            logging.exception("An error occurred during request handling")
 
 if __name__ == "__main__":
     #server_address = ('', 5000)
