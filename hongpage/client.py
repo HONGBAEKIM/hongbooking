8c42231618cfc1e88c8bea6a2609c42d196737a9
Ubuntu <ubuntu@ip-172-31-31-55.eu-north-1.compute.internal>
2024-03-22 00:32:37 +0000

    delete client.py

--- hongpage/client.py
+++
@@ -1,9 +0,0 @@
-import requests  # Import requests library
-
-# Function to send GET request to /hello endpoint
-def send_hello_request():
-    response = requests.get('http://localhost:5000/hello/John')
-    print(response.json())
-
-if __name__ == "__main__":
-    send_hello_request()
