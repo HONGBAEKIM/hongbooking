dcf1142634a7ee5fb1617a3ef5fc8197ecaddf81
HONGBAEKIM <hongpei17@gmail.com>
2024-03-22 01:26:38 +0100

    server.js

--- hongpage/server.js
+++ hongpage/server.js
@@ -119,7 +119,9 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// Serve your static files (HTML, CSS, etc.)
+app.use(express.static('/var/www/html/static'));  
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
